c2180c9c5c20936ce274f599d76b36dd80b52978
AncientFarmingEquipment <109826361+AncientFarmingEquipment@users.noreply.github.com>
2023-02-13 15:22:24 +0200

    added cv

---
+++ src/Schemas/Data.js
@@ -0,0 +1,123 @@
+export const Data = {
+  profile: {
+    name: "Andrii Shapoval",
+    ocupation: "Front-end developer",
+    location: "Kyiv, Ukraine",
+    email: "andrey1995ua@gmail.com",
+    telephone: "+380957488872",
+    image: "images/90208122_10222269974249078_3631065450546200576_n.jpg",
+  },
+  aboutMe: {
+    label: "About me",
+    description: "I am a Front-End developer looking for a perspective and interesting job with possibility of professional and personal growth. I focus on HTML, CSS, JavaScript, and React. For me, it is important to collaborate closely with the team to solve issues and deliver a high-quality product. I am responsible, honest and ready for challenging tasks",
+  },
+  skills: {
+    technicalLabel: "Hard skills",
+    softLabel: "Soft skills",
+    technicalSkills: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "React.js",
+      "Redux",
+      "Node.js",
+      "MySQL",
+      "MongoDB",
+      "Gulp",
+      "Bootstrap",
+    ],
+    softSkills: [
+      "Communication skills",
+      "Teamwork",
+      "Problem solving",
+      "Analytical thinking",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Andrii Shapoval",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/andrii-shapoval-045196232",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Andrii Shapoval",
+        name: "github",
+        url: "https://gitlab.com/AncientFarmingEquipment",
+        className: "bxl-github",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Freelance Web Developer",
+        period: "2010 - Present",
+        company: "",
+        description: [
+
+        ],
+      },
+      {
+        title: "Civil officer",
+        period: "Jun 2016 - Jun 2017",
+        company: "State criminal-Executive service of Ukraine",
+        description: [
+
+        ],
+      },
+      {
+        title: "Painting machine engineer",
+        period: "Dec 2017 - Mar 2018",
+        company: "BORNE FURNITURE SP. Z O.O., Gorzów Wielkopolski",
+        description: [
+
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Front end developer. DAN.IT Education",
+        date: "Jan. 2022 - Jan. 2023",
+        institution: "DAN.IT, Kyiv",
+      },
+      {
+        career: "Bachelor of Laws",
+        date: "Sep. 2013 - Jul. 2017",
+        institution: "University of Modern Knowledge, Vinnytsya",
+      }
+    ],
+    proyects: [
+      {
+        name: "Bakery shop",
+        period: "Sep. 2022 - Present",
+        description: [
+          "The main objective of this project was to acquire teamwork skills. The skill of controlling the workflow, communication with the team, the customer, the skill of presenting the project infront of a large audience. In addition to my role, I designed add to basket form and organized a repository on GitHub",
+        ],
+      },
+      {
+        name: "Twitter clone",
+        period: "Jun. 2022",
+        description: [
+          "The essence of the project was the gradual development of technologies such as JavaScript, HTML5 and CSS3 in addition, familiarization with the SASS",
+        ],
+      },
+      {
+        name: "React realtime chat",
+        period: "Sep. 2022",
+        description: [
+          "An application for conversation between two or more people.",
+        ],
+      },
+      {
+        name: "NotOnlyFans",
+        period: "Jul. 2022",
+        description: [
+          "Basically online store with fans and old used TVs",
+        ],
+      },
+    ],
+  },
+};
